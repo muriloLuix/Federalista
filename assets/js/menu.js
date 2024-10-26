5728b58d44e56b11faeff35555358a81651e65c8
Murilo Luiz Jaboinski <muriloluiz654@gmail.com>
2024-10-26 12:22:47 -0300

    Ajustando responsividade

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,8 +1,32 @@
 function toggleMenu() {
     const hamburger = document.querySelector('.hamburger');
-    const navigation = document.querySelector('.navigation');
     const headerItens = document.querySelector('.headerItens');
+    const body = document.body; // Seleciona o elemento body
+
 
     hamburger.classList.toggle('active');
-    navigation.classList.toggle('open');
+
+
+    headerItens.classList.toggle('active');
+
+    // Adiciona ou remove a classe 'no-scroll' ao body
+    if (headerItens.classList.contains('active')) {
+        body.classList.add('no-scroll');
+    } else {
+        body.classList.remove('no-scroll');
+    }
 }
+
+// Adiciona evento de clique a cada item do menu
+const menuItems = document.querySelectorAll('.headerItens ul a');
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        const headerItens = document.querySelector('.headerItens');
+        const hamburger = document.querySelector('.hamburger');
+        const body = document.body;
+
+        headerItens.classList.remove('active');
+        hamburger.classList.remove('active');
+        body.classList.remove('no-scroll');
+    });
+});
