f909aef7bb142675704d70dce38f5413f9f343de
Murilo Luiz Jaboinski <muriloluiz654@gmail.com>
2024-12-16 00:17:27 -0300

    Atualizado textos conforme orientação

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -1,5 +1,5 @@
 function copiarCodigo() {
-    const codigo = "00020126390014br.gov.bcb.pix0117contato@if.org.br5204000053039865802BR5925INSTITUTO FEDERALISTA IFB6008CURITIBA62250521s10AOsVEU3XiUFHEsxwfE63047EB1";
+    const codigo = "00020126520014br.gov.bcb.pix0130eucontribuo@federalista.org.br5204000053039865802BR5925INSTITUTO FEDERALISTA IFB6008CURITIBA62250521s10AOsVEU3XiUFHEsxwfE6304B08D";
     navigator.clipboard.writeText(codigo).then(() => {
         alert("Código copiado para a área de transferência!");
     });
