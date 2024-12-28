6661596891dc70b0a78d25519ba3f425be6b9fff
Murilo Luiz Jaboinski <muriloluiz654@gmail.com>
2024-12-28 17:54:05 -0300

    Ajustando informações Thomas

--- assets/js/formated.js
+++ assets/js/formated.js
@@ -12,8 +12,9 @@ function formatRG() {
         .replace(/\D/g, '') // Remove tudo que não é dígito
         .replace(/(\d{2})(\d)/, '$1.$2') // Adiciona ponto após os dois primeiros dígitos
         .replace(/(\d{3})(\d)/, '$1.$2') // Adiciona ponto após os próximos três dígitos
-        .replace(/(\d{3})(\d)$/, '$1-$2'); // Adiciona hífen antes do último dígito
+        .replace(/(\d{3})(\d{1,2})$/, '$1-$2'); // Adiciona hífen antes dos dois últimos dígitos
 }
+
 
 function formatContribuicao() {
     const contribuicao = document.getElementById('contribuicao');
@@ -22,4 +23,20 @@ function formatContribuicao() {
     valor = valor.replace('.', ','); // Substitui ponto por vírgula
     valor = 'R$ ' + valor.replace(/\B(?=(\d{3})+(?!\d))/g, '.'); // Adiciona pontos como separador de milhar
     contribuicao.value = valor;
+
+    // Adiciona a classe 'used' caso o campo tenha valor
+    if (contribuicao.value.trim() !== '') {
+        contribuicao.classList.add('used');
+    } else {
+        contribuicao.classList.remove('used');
+    }
+}
+
+function setContribuicao(valor) {
+    const contribuicao = document.getElementById('contribuicao');
+    contribuicao.value = (valor / 100).toFixed(2).replace('.', ','); // Define o valor no formato de moeda
+    contribuicao.value = 'R$ ' + contribuicao.value.replace(/\B(?=(\d{3})+(?!\d))/g, '.'); // Formata com pontos
+
+    // Adiciona a classe 'used' para o floating label subir
+    contribuicao.classList.add('used');
 }
