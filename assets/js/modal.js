41feeed042684f6aab01616b21c334183a2fc5b5
Murilo Luiz Jaboinski <muriloluiz654@gmail.com>
2024-12-28 23:12:58 -0300

    Ajustando itens Thomas

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,25 +1,17 @@
 document.getElementById("myForm").addEventListener("submit", function (event) {
-    event.preventDefault();
+    event.preventDefault(); // Evita o envio padrão do formulário
 
-    // Mostra o modal de agradecimento
+    // Seleciona o modal
     const modal = document.getElementById("thankYouModal");
+
+    // Mostra o modal e adiciona animação
     modal.style.display = "block";
-    modal.classList.add("show");
-
-    // Aguarda um pouco e então envia o formulário
-    setTimeout(() => {
-        event.target.submit();
-    }, 1000);
-
-    setTimeout(() => {
-        window.location.href = "https://www.federalista.org.br";
-    }, 1500); // Ajusta o tempo para garantir que o formulário seja enviado antes do redirecionamento
+    modal.querySelector(".modal-content").classList.add("show");
 });
-
 
 // Função para fechar o modal
 function closeModal() {
     const modal = document.getElementById("thankYouModal");
     modal.style.display = "none";
-    modal.classList.remove("show");
+    modal.querySelector(".modal-content").classList.remove("show");
 }
